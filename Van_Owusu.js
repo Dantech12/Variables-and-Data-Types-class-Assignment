3e8b18da8c587a94dc29280c2dde7162e503c3ea
Dantech12 <owusuvan374@gmail.com>
2023-07-07 05:58:36 -0700

    Done

---
+++ Van_Owusu.js
@@ -0,0 +1,17 @@
+var numOne=7;
+var numTwo=3;
+var numThree=5;
+
+var Result=numOne+numTwo+numThree;
+console.log(Result)
+
+
+var fullName="Emmanuel Van Owusu"
+var age=24;
+var isStudent=true;
+var hobbies=["Watching movies","creating robots","like coding"]
+
+console.log(fullName)
+console.log(age)
+console.log(isStudent)
+console.log(hobbies)
